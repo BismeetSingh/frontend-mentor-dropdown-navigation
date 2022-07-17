b313713b1969c3af70edb64ac414838679d754dc
bissu <bismeetsingh352@gmail.com>
2022-07-17 19:39:17 +0530

    Updated styles to float menu from right on mobile

--- toggle.js
+++ toggle.js
@@ -23,8 +23,6 @@ const toggleMenuVisibility = (index) => {
 const showMenu = (toggleId,navId) =>{
     const toggle = document.getElementById(toggleId);
     const navMenu = document.getElementById(navId);
-    console.log(toggle);
-    console.log(navMenu);
     if (toggle && navMenu){
         toggle.addEventListener('click',()=>{
             navMenu.classList.toggle('show');
