49152c2fddc3c241bf5c7103e276763e4a99729e
bissu <bismeetsingh352@gmail.com>
2022-07-17 11:51:16 +0530

    Added login and register.

--- toggle.js
+++ toggle.js
@@ -1,15 +1,14 @@
-const toggleMenuVisibility = (index) => {
-    document.getElementsByClassName("dropdown")[index].classList.toggle("show");
-    let img = document.getElementsByClassName('toggle-menu')[index].src;
-    if (img.indexOf('images/icon-arrow-down.svg') != -1) {
-        document.getElementsByClassName('toggle-menu')[index].src = 'images/icon-arrow-up.svg';
-    }
-    else {
-        document.getElementsByClassName('toggle-menu')[index].src = 'images/icon-arrow-down.svg';
-    }
+// const toggleMenuVisibility = (index) => {
+//     document.getElementsByClassName("dropdown")[index].classList.toggle("show");
+//     let img = document.getElementsByClassName('toggle-menu')[index].src;
+//     if (img.indexOf('images/icon-arrow-down.svg') != -1) {
+//         document.getElementsByClassName('toggle-menu')[index].src = 'images/icon-arrow-up.svg';
+//     }
+//     else {
+//         document.getElementsByClassName('toggle-menu')[index].src = 'images/icon-arrow-down.svg';
+//     }
 
-}
-
+// }
 
 const showMenu = (toggleId,navId) =>{
     const toggle = document.getElementById(toggleId);
